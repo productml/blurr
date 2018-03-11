5645f8ed1be764d1630862e696328db2b3239ee5
Miguel Serrano <mserranom@users.noreply.github.com>
2018-03-11 15:11:25 +0000

    Update build.sh

--- blurr/build.sh
+++ blurr/build.sh
@@ -11,3 +11,6 @@ pipenv run yapf -i -r *.py */**.py --style='{allow_split_before_dict_value : fal
 echo "building package..."
 pipenv run python setup.py sdist
 pipenv run python setup.py bdist_wheel
+
+
+echo "Done."
