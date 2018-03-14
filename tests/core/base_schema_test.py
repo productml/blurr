9b1126ffc889156604cb2a7057219e573ea381f2
miguel <miguel@productml.com>
2018-03-14 21:28:13 +0000

    added yamale checks

--- tests/core/base_schema_test.py
+++ tests/core/base_schema_test.py
@@ -42,17 +42,46 @@ def test_base_schema_valid(test_schema_spec: Dict[str, Any]) -> None:
 
 
 def test_base_schema_empty() -> None:
-    with raises(InvalidSchemaError, Message='Required attribute missing.'):
+    with raises(InvalidSchemaError) as err:
         TestSchema({})
+    assert "Error validating data spec with schema TestSchema" in str(
+        err.value)
+    assert "Name: Required field missing" in str(err.value)
+    assert "Type: Required field missing" in str(err.value)
 
 
 def test_base_schema_name_missing(test_schema_spec: Dict[str, Any]) -> None:
     del test_schema_spec[BaseSchema.ATTRIBUTE_NAME]
-    with raises(InvalidSchemaError, Message='Required attribute missing.'):
+    with raises(InvalidSchemaError) as err:
         TestSchema(test_schema_spec)
+    assert "Error validating data spec with schema TestSchema" in str(
+        err.value)
+    assert "Name: Required field missing" in str(err.value)
 
 
 def test_base_schema_type_missing(test_schema_spec: Dict[str, Any]) -> None:
     del test_schema_spec[BaseSchema.ATTRIBUTE_TYPE]
-    with raises(InvalidSchemaError, Message='Required attribute missing.'):
+    with raises(InvalidSchemaError) as err:
         TestSchema(test_schema_spec)
+    assert "Error validating data spec with schema TestSchema" in str(
+        err.value)
+    assert "Type: Required field missing" in str(err.value)
+
+
+def test_base_schema_with_attribute_when_missing_is_valid(
+        test_schema_spec: Dict[str, Any]) -> None:
+    del test_schema_spec[BaseSchema.ATTRIBUTE_WHEN]
+    TestSchema(test_schema_spec)
+    test_schema = TestSchema(test_schema_spec)
+    assert test_schema.name == test_schema_spec[BaseSchema.ATTRIBUTE_NAME]
+    assert test_schema.type == test_schema_spec[BaseSchema.ATTRIBUTE_TYPE]
+    assert test_schema.when is None
+
+
+def test_invalid_type_raises_error(test_schema_spec: Dict[str, Any]) -> None:
+    test_schema_spec[BaseSchema.ATTRIBUTE_TYPE] = "foo"
+    with raises(InvalidSchemaError) as err:
+        TestSchema(test_schema_spec)
+    assert "Error validating data spec with schema TestSchema" in str(
+        err.value)
+    assert "Type: 'foo' is not a DTC Valid Type" in str(err.value)
