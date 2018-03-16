221dbdd3fd432f3a8fd35f502bae0e76670fb46a
miguel <miguel@productml.com>
2018-03-16 10:03:53 +0000

    removed unnecesary file

--- blurr/util/eprint.py
+++
@@ -1,9 +0,0 @@
-from __future__ import print_function
-import sys
-
-
-def eprint(*args, **kwargs):
-    """
-       Same as print(), but prints output to stderr instead of stdout
-    """
-    print(*args, file=sys.stderr, **kwargs)
