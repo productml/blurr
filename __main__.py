e4ef9c0eda33d438a4971d138d84719a2651a12f
Miguel Serrano <mserranom@gmail.com>
2018-03-27 18:48:52 +0100

    CLI reads version

--- __main__.py
+++ __main__.py
@@ -14,8 +14,15 @@ from blurr.cli.cli import cli
 from blurr.util.out import Out
 
 
+def read_version():
+    version_file = open("blurr/VERSION","r")
+    version = version_file.readline()
+    version_file.close()
+    return version
+
+
 def main():
-    arguments = docopt(__doc__, version='pre-alpha')
+    arguments = docopt(__doc__, version=read_version())
     result = cli(arguments, Out())
     sys.exit(result)
 
