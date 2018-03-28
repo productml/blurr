9c1682bcd8b0d24b11966361bd0260e423a7bdc9
Indradeep Biswas <indradeep@productml.com>
2018-03-28 14:37:23 -0700

    Reinstating Simple Field that got accidentally deleted

--- blurr/core/simple_fields.py
+++ blurr/core/simple_fields.py
@@ -52,3 +52,10 @@ class DateTimeFieldSchema(FieldSchema):
     @property
     def default(self) -> Any:
         return None
+
+
+class SimpleField(Field):
+    """
+    Represents a simple field that can be of any native feild type
+    """
+    pass
