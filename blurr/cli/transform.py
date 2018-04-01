a0ddf003d76418a43893c2b7cecaa4657c470177
Prashast Khandelwal <prashast@productml.com>
2018-04-01 13:58:12 -0700

    Added transform unit tests.

--- blurr/cli/transform.py
+++ blurr/cli/transform.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from blurr.cli.out import Out
 from blurr.cli.util import get_stream_window_dtc_files, get_yml_files
@@ -6,18 +6,20 @@ from blurr.cli.validate import get_valid_yml_files
 from blurr.runner.local_runner import LocalRunner
 
 
-def transform(stream_dtc_file: str, window_dtc_file: str,
+def transform(stream_dtc_file: Optional[str], window_dtc_file: Optional[str],
               raw_json_files: List[str], out: Out) -> int:
     if stream_dtc_file is None and window_dtc_file is None:
         stream_dtc_file, window_dtc_file = get_stream_window_dtc_files(
             get_valid_yml_files(get_yml_files()))
 
-    if stream_dtc_file is None and window_dtc_file is not None:
-        out.eprint(
-            'Streaming DTC needs to be provided if a Window DTC is provided.')
+    if stream_dtc_file is None:
+        out.eprint('Streaming DTC file not provided and could not be found in '
+                   'the current directory.')
         return 1
 
     local_runner = LocalRunner(raw_json_files, stream_dtc_file,
                                window_dtc_file)
     local_runner.execute()
     local_runner.print_output()
+
+    return 0
