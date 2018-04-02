b72d5372566b113d2ca1819d4e092df0c91c90a3
Indradeep Biswas <indradeep@productml.com>
2018-04-02 16:48:12 -0700

    Removed debug print from evaluation

--- blurr/core/evaluation.py
+++ blurr/core/evaluation.py
@@ -114,7 +114,6 @@ class Expression:
         try:
             return eval(self.code_object, evaluation_context.global_context,
                         evaluation_context.local_context)
-        except Exception as e:
-            print(e)
+        except:
             # TODO Log exception
             return None
