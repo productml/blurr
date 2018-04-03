60fcfedee5e8f3a87da68729e6871a33ad69bb1c
Prashast Khandelwal <prashast@productml.com>
2018-04-03 14:31:34 -0700

    Add source to transform CLI.

--- blurr/cli/cli.py
+++ blurr/cli/cli.py
@@ -9,6 +9,10 @@ def cli(arguments: Dict[str, Any], out: Out) -> int:
     if arguments['validate']:
         return validate_command(arguments['<DTC>'], out)
     elif arguments['transform']:
+        source = []
+        if arguments['--source'] is not None:
+            source = arguments['--source'].split(',')
+        elif arguments['<raw-json-files>'] is not None:
+            source = arguments['<raw-json-files>'].split(',')
         return transform(arguments['--streaming-dtc'],
-                         arguments['--window-dtc'],
-                         arguments['<raw-json-files>'], out)
+                         arguments['--window-dtc'], source, out)
