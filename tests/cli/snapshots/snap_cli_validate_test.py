101cf34cf6ccda270b1ed2b26ae86a637caaae50
Prashast Khandelwal <prashast@productml.com>
2018-04-03 11:35:41 -0700

    Use file path from module for the schema definition files.

--- tests/cli/snapshots/snap_cli_validate_test.py
+++ tests/cli/snapshots/snap_cli_validate_test.py
@@ -7,6 +7,6 @@ from snapshottest import Snapshot
 snapshots = Snapshot()
 
 snapshots['test_invalid_dtc 1'] = '''
-Error validating data dtc with schema blurr/core/syntax/dtc_streaming_schema.yml
+Error validating data dtc with schema
 \tVersion: '2088-03-01' not in ('2018-03-01',)
 '''
